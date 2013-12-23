f2ffa9852ad04a53a926387d75a2e0ffc113dab3
ihcfan <ihcfan@braincookie.com>
2013-12-23 14:42:12 -0500

    Replaced Websockets library with libwebsockets.

---
+++ NautiHubMapServer/main.h
@@ -0,0 +1,14 @@
+//
+//  main.h
+//  NautiHubMapServer
+//
+//  Created by Keith Ballantyne on 12/23/13.
+//  Copyright (c) 2013 Keith Ballantyne. All rights reserved.
+//
+
+#ifndef __NautiHubMapServer__main__
+#define __NautiHubMapServer__main__
+
+#include <iostream>
+
+#endif /* defined(__NautiHubMapServer__main__) */
